2c619a5eb631c74e04940d198dbd396bf371cb25
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-01-31 04:56:29 +0300

    added views for index and homepage

--- highlights/views.py
+++ highlights/views.py
@@ -1,3 +1,17 @@
+import http
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def  index(request):
+    return HttpResponse('Welcome to Momments')
+
+def homepage(request):
+    html = f'''
+            <html>
+                <body>
+                <h1>homepage</h1>
+                </body>
+            </html>
+         '''
+    return HttpResponse(html)
