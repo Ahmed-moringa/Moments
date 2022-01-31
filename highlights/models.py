807e6fce4681b2019a194f47d5fd9814a243cfe7
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-01-31 14:37:48 +0300

    added methods for image, category and location

--- highlights/models.py
+++ highlights/models.py
@@ -4,14 +4,35 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=50)
 
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+    
     def __str__(self):
         return self.name
+    
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+    
+    def save_category(self):
+        self.save()
+    
+    def delete_category(self):
+        self.delete()
 
 class Image(models.Model):
     
@@ -20,7 +41,31 @@ class Image(models.Model):
     description = models.TextField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
  
     def  __str__(self):
-        return self.name
+        return self.name_
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
