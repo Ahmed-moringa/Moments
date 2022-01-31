2744ba08042a0f86e37a0fa0d3328a208f9dd3b2
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-01-31 14:38:14 +0300

    created url for search and location

--- highlights/urls.py
+++ highlights/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from .views import (
     index,
-    homepage
+    search_results
     )
 
 urlpatterns=[
     path('', index, name = "index"),
-    path('home/', homepage, name = 'homepage')
+    path('search/', search_results, name = 'search'),
+    re_path(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
