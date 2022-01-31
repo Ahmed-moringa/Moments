6817a44d718cb8d776a2f7976a64f098b9473258
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-01-31 06:16:18 +0300

    added bootstrap

--- momments/settings.py
+++ momments/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
+    'highlights',
+    # third party libraries
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
