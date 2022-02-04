90fe23e58274039d1dc8fa848129018d2369d06d
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-02-04 07:55:51 +0300

    fixed error with whitenoise

--- momments/settings.py
+++ momments/settings.py
@@ -133,9 +133,8 @@ STATIC_URL = '/static/'
 #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'momments/static')
-]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
